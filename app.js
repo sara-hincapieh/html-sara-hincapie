1d2f5706cd2d8ccefd893fe38ec6b2d3d171d119
sara-hincapieh <123768467+sara-hincapieh@users.noreply.github.com>
2023-02-13 12:36:38 -0500

    Variables

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
 document.addEventListener("DOMContentLoad", () => {});
 const cardAdj = [
-{
-  name: "cab.jpg",
-  img: "images/cab.jpg"
-},
+  { name: "cab.jpg", img: "images/cab.jpg" },
+  { name: "can.jpg", img: "images/can.jpg" },
+  { name: "del.jpg", img: "images/del.jpg" },
+  { name: "eso.jpg", img: "images/eso.jpg" },
+  { name: "est.jpg", img: "images/est.jpg" },
+  { name: "tib.jpg", img: "images/tib.jpg" }
+];
 
- ];
-
-
-
-
+const cuadricula = document.querySelector(".cuadricula");
+const resultado = document.querySelector("#resultado");
+var cartasescogidas = [];
+var cartasescogidasid = [];
+var catasGanadas = [];
